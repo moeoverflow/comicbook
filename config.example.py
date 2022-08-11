df42b17043fa395a767f84a3464e8c3886535bfa
everpcpc <git@everpcpc.com>
2022-08-11 22:29:43 +0800

    chore: upgrade dependencies

--- config.example.py
+++ config.example.py
@@ -1,29 +1,24 @@
 import os
 from enum import Enum
 
-URL = 'https://comic.moeoverflow.com'
+URL = "https://comic.moeoverflow.com"
 
-CORS_ALLOWED_ORIGINS = [
-    URL,
-    "https://nhentai.net",
-    "https://e-hentai.org/",
-    "https://wnacg.org/"
-]
+CORS_ALLOWED_ORIGINS = [URL, "https://nhentai.net", "https://e-hentai.org/", "https://wnacg.org/"]
 
 DEBUG = False
 
-TELEGRAM_BOT_TOKEN = ''
+TELEGRAM_BOT_TOKEN = ""
 
-SENTRY_DSN = ''
+SENTRY_DSN = ""
 
 PROXY = {
     # 'http': 'socks5://127.0.0.1:1080',
     # 'https': 'socks5://127.0.0.1:1080'
 }
 
-CALIBRE_LIBRARY_PATH = ''
+CALIBRE_LIBRARY_PATH = ""
 
-MONGODB_URL = 'mongodb://localhost:27017/'
+MONGODB_URL = "mongodb://localhost:27017/"
 
 
 class DOMAIN(Enum):
@@ -33,23 +28,26 @@ class DOMAIN(Enum):
     none = "none"
 
 
-COMIC_ROOT_PATH = '/storage/comic'
+COMIC_ROOT_PATH = "/storage/comic"
 
 # ------------------------------------------------------------
 
 COMIC_MAIN_PATH = os.path.abspath(os.curdir) + COMIC_ROOT_PATH
 COMIC_PATHS = {
-    DOMAIN.nhentai_net: '%s/%s' % (COMIC_MAIN_PATH, DOMAIN.nhentai_net.value),
-    DOMAIN.ehentai_org: '%s/%s' % (COMIC_MAIN_PATH, DOMAIN.ehentai_org.value),
-    DOMAIN.wnacg_org: '%s/%s' % (COMIC_MAIN_PATH, DOMAIN.wnacg_org.value)
+    DOMAIN.nhentai_net: "%s/%s" % (COMIC_MAIN_PATH, DOMAIN.nhentai_net.value),
+    DOMAIN.ehentai_org: "%s/%s" % (COMIC_MAIN_PATH, DOMAIN.ehentai_org.value),
+    DOMAIN.wnacg_org: "%s/%s" % (COMIC_MAIN_PATH, DOMAIN.wnacg_org.value),
 }
 COMIC_DOWNLOADING_PATHS = {
-    DOMAIN.nhentai_net: '%s/downloading' % (COMIC_PATHS[DOMAIN.nhentai_net]),
-    DOMAIN.ehentai_org: '%s/downloading' % (COMIC_PATHS[DOMAIN.ehentai_org]),
-    DOMAIN.wnacg_org: '%s/downloading' % (COMIC_PATHS[DOMAIN.wnacg_org]),
+    DOMAIN.nhentai_net: "%s/downloading" % (COMIC_PATHS[DOMAIN.nhentai_net]),
+    DOMAIN.ehentai_org: "%s/downloading" % (COMIC_PATHS[DOMAIN.ehentai_org]),
+    DOMAIN.wnacg_org: "%s/downloading" % (COMIC_PATHS[DOMAIN.wnacg_org]),
 }
 DOWNLOAD_URL = {
-    DOMAIN.nhentai_net: '/comic/download/nhentai-{params[id]}.epub',
-    DOMAIN.ehentai_org: '/comic/download/ehentai-{params[id]}.epub',
-    DOMAIN.wnacg_org: '/comic/download/wnacg-{params[id]}.epub'
+    DOMAIN.nhentai_net: "/comic/download/nhentai-{params[id]}.epub",
+    DOMAIN.ehentai_org: "/comic/download/ehentai-{params[id]}.epub",
+    DOMAIN.wnacg_org: "/comic/download/wnacg-{params[id]}.epub",
+}
+COOKIES = {
+    DOMAIN.nhentai_net: {},
 }
