207a38b9eec1e96c99dcd418bae8c77e46eac9ba
everpcpc <git@everpcpc.com>
2023-02-10 19:45:57 +0800

    refactor(web): replace flask with fastapi

--- comicbook.py
+++ comicbook.py
@@ -6,24 +6,22 @@ import getopt
 from crawler.__init__ import Crawler
 
 import config
-import webapp
 from bot import ComicbookTelegramBot
 
 
-version = '1.1.0'
+version = "1.1.0"
 
 if __name__ == "__main__":
 
-    help = '''comicbook options:
+    help = """comicbook options:
       -h, --help            Show help.
       -v, --version         Show version and exit.
       -c, --comic           a comic link on > nhentai.net
                                             > e-hentai.org
                                             > wnacg.org
       -o, --output          Specify a output path.(temporarily disabled)
-      -s, --server          Run flask web server.
       -t, --telegram-bot    Run telegram bot.
-    '''
+    """
 
     link = ""
     output = ""
@@ -33,7 +31,9 @@ if __name__ == "__main__":
         sys.exit()
     argv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(argv, "hvc:o:ts", ["help", "version", "comic=", "output=", "telegram-bot", "server"])
+        opts, args = getopt.getopt(
+            argv, "hvc:o:ts", ["help", "version", "comic=", "output=", "telegram-bot", "server"]
+        )
     except getopt.GetoptError:
         print(help)
         sys.exit()
@@ -51,8 +51,3 @@ if __name__ == "__main__":
     if ("-t", "") in opts or ("--telegram-bot", "") in opts:
         bot = ComicbookTelegramBot(config.TELEGRAM_BOT_TOKEN)
         bot.start()
-
-    if ("-s", "") in opts or ("--server", "") in opts:
-        webapp.app.debug = config.DEBUG
-        webapp.socketio.run(webapp.app)
-
---
+++ webapp/utils.py
@@ -0,0 +1,25 @@
+import config
+from crawler.utils.storage import Storage
+from db.mongodb import comicbook_calibre
+
+
+def which_type(type):
+    if type == "nhentai":
+        return config.DOMAIN.nhentai_net
+    elif type == "ehentai":
+        return config.DOMAIN.ehentai_org
+    elif type == "wnacg":
+        return config.DOMAIN.wnacg_org
+    else:
+        return None
+
+
+def comic_file(domain, id):
+    result = comicbook_calibre.find_one({"domain": domain.value, "id": id})
+    if result is None:
+        return None
+    else:
+        if result["storeInCalibre"]:
+            return Storage.get_calibre_epub_file(result["filepath"])
+        else:
+            return Storage.get_comicbook_epub_file(result["filepath"])
